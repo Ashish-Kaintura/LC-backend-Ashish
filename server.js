ac6bead9b3495b8a92e068e8e88a142a8487bf91
Ashish-Kaintur <ashu88793@gmail.com>
2025-07-30 13:09:12 +0530

    first commit

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.json());
 // Connect to MongoDB
 mongoose
   .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log("✅ MongoDB connected"))
+  .then(() => console.log("MongoDB connected"))
   .catch((err) => console.error("Mongo error:", err));
 
 app.post("/api/store-location", async (req, res) => {
